1659970c669deb9c4433bc57d064ce708b2bfddd
mtobeery <64616989+mtobeery@users.noreply.github.com>
2025-06-25 11:02:59 +0300

    Create kernel_shared.h

---
+++ kernel/kernel_shared.h
@@ -0,0 +1,49 @@
+// kernel_shared.h - AiOS Unified Kernel Shared Structures and Definitions
+
+#ifndef KERNEL_SHARED_H
+#define KERNEL_SHARED_H
+
+#include <Uefi.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+
+#include "ai_core.h"
+#include "telemetry_mind.h"
+#include "trust_mind.h"
+#include "entropy_mind.h"
+
+// ==================== Constants ====================
+
+#define CPU_PHASE_COUNT        150
+#define MEMORY_PHASE_COUNT     150
+#define TOTAL_PHASE_COUNT      (CPU_PHASE_COUNT + MEMORY_PHASE_COUNT)
+
+#define CPU_PHASE_THRESHOLD    5000000
+#define MEMORY_PHASE_THRESHOLD 4000000
+
+#define CPU_PHASE_MAX_LOAD     10000000
+#define MEMORY_ENTROPY_SALT    0x1A2B3C4D
+
+// ==================== Shared State ====================
+
+typedef struct {
+    UINTN total_phases;
+    UINT64 trust_score;
+    UINT64 cpu_elapsed_tsc[CPU_PHASE_COUNT + 1];
+    UINT8 cpu_missed[CPU_PHASE_COUNT + 1];
+    UINT64 memory_elapsed_tsc[MEMORY_PHASE_COUNT + 1];
+    UINT8 memory_missed[MEMORY_PHASE_COUNT + 1];
+
+    // Memory-specific fields
+    VOID *MemoryMap;
+    UINTN MemoryMapSize;
+    UINTN DescriptorSize;
+    UINT32 DescriptorVersion;
+    UINTN DescriptorCount;
+    UINT64 EntropyScore;
+    UINTN MissCount;
+} KERNEL_CONTEXT;
+
+#endif // KERNEL_SHARED_H
